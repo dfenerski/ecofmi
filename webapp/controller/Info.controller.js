fb232c28ece9a6bb47b5ef78d681e7278cd251a2
Dimitar Fenerski <dimitar.fenerski@berncon.com>
2020-10-19 18:47:36 +0200

    commit initializes planned app views

---
+++ webapp/controller/Info.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define(["./BaseController"], function (BaseController) {
+  "use strict";
+
+  return BaseController.extend("fmi.Eco.controller.Info", {
+    onInit: function () {},
+  });
+});
