6abfc39df39b7829d553bd2e8485b10577cdf57c
dfenerski <dfenerski@gmail.com>
2020-10-25 11:10:05 +0200

    commit implements game integration and adds a gameover event. See https://github.com/NikolaHristov98/EcoProject for source code of the game

--- webapp/controller/Play.controller.js
+++ webapp/controller/Play.controller.js
@@ -1,4 +1,4 @@
-sap.ui.define(["./BaseController"], function (BaseController) {
+sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
   "use strict";
 
   return BaseController.extend("fmi.Eco.controller.Play", {
@@ -10,5 +10,32 @@ sap.ui.define(["./BaseController"], function (BaseController) {
           oLocal.setProperty("/menu/currentView", "play");
         });
     },
+    onAfterRendering: function () {
+      const oCanvas = document.getElementById("canvas");
+      oCanvas.addEventListener("gameover", (oEvent) => {
+        MessageBox.show(
+          `Your final score is ${oEvent.detail}. Press OK to play again!`, {
+          icon: MessageBox.Icon.INFORMATION,
+          title: "Oh no! You ran out of lives.",
+          actions: [MessageBox.Action.OK],
+          emphasizedAction: MessageBox.Action.OK,
+          onClose: function (oAction) {
+            location.reload();
+          }
+        }
+        );
+      })
+      $.loadScript = function (url, callback) {
+        $.ajax({
+          url: url,
+          dataType: 'script',
+          success: callback,
+          async: true
+        });
+      }
+      $.loadScript("lib/game/min.js", () => {
+        main();
+      });
+    }
   });
 });
